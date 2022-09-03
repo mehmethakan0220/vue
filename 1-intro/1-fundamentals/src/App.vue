89ecfdefa29153419fde2e4ca3fd6b2822f6d5fb
mehmethakan0220 <mehmethakan0220@gmail.com>
2022-09-03 19:25:12 +0300

    Attribute bindings, v-bind

--- 1-intro/1-fundamentals/src/App.vue
+++ 1-intro/1-fundamentals/src/App.vue
@@ -4,11 +4,10 @@
   <h2>{{rawHtml}}</h2>
   <h2><span v-html="rawHtml"></span></h2>
 
+  <div v-bind:id="dynamicId"> If the bound value is null or undefined, 
+    then the attribute will be removed from the rendered element. try as both null and undefined</div>
+
 </template>
-
-
-
-
 
 <script>
 
@@ -16,7 +15,10 @@ export default  {
   data(){
     return{
       msg:'hello vue',
-      rawHtml:'<div>this is raw html</div>'
+      rawHtml:'<div>this is raw html</div>',
+      dynamicId:112,/*
+      The v-bind directive instructs Vue to keep the element's id attribute in sync with the component's dynamicId property. If the bound value is null or undefined, then the attribute will be removed from the rendered element.
+      */
     }
   }
 };
