c5f2a3dfb3dbe91afac92c3fc2984b46b8402768
Hakan Yalin <Hakan.Yalin@logo.com.tr>
2022-09-03 18:02:59 +0300

    v-html directive

--- 1-intro/1-fundamentals/src/App.vue
+++ 1-intro/1-fundamentals/src/App.vue
@@ -1,6 +1,8 @@
 <template>
 
   <h1>{{msg}}</h1>
+  <h2>{{rawHtml}}</h2>
+  <h2><span v-html="rawHtml"></span></h2>
 
 </template>
 
@@ -13,7 +15,8 @@
 export default  {
   data(){
     return{
-      msg:'hello vue'
+      msg:'hello vue',
+      rawHtml:'<div>this is raw html</div>'
     }
   }
 };
