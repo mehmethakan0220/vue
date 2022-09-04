00b81ab0a7dbd5efaa3448423a1838454abd4171
mehmethakan0220 <mehmethakan0220@gmail.com>
2022-09-04 19:07:14 +0300

    declaring reactive state

--- 1-intro/2-reactivity/src/App.vue
+++ 1-intro/2-reactivity/src/App.vue
@@ -2,6 +2,12 @@
   <button @click="inc">Click to increment</button>
   <button @click="dec">Click to decrement</button>
   <div>result: {{count}}</div>
+
+  <hr>
+  <div>It is possible to add a new property directly to this without including it in data. However, properties added this way will not be able to trigger reactive updates.
+    <hr>
+    Vue uses a $ prefix when exposing its own built-in APIs via the component instance. It also reserves the prefix _ for internal properties. You should avoid using names for top-level data properties that start with either of these characters.
+  </div>
 </template>
 
 
@@ -9,8 +15,11 @@
 export default{
   data(){
     return{
-      count:0,
+      count:null,
     }
+  },
+  mounted(){
+    this.count = 100;
   },
   methods:{
     inc(){
