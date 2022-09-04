5b8323f0a2d485f0f2b9edca70e9b60934fdb748
mehmethakan0220 <mehmethakan0220@gmail.com>
2022-09-04 18:57:11 +0300

    basic reactivity

---
+++ 1-intro/2-reactivity/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="inc">Click to increment</button>
+  <button @click="dec">Click to decrement</button>
+  <div>result: {{count}}</div>
+</template>
+
+
+<script>
+export default{
+  data(){
+    return{
+      count:0,
+    }
+  },
+  methods:{
+    inc(){
+      this.count ++;
+    },
+    dec(){
+      this.count--;
+    }
+  }
+}
+
+</script>
+
+
+<style>
+
+
+</style>
