b67e81d7ea40e3cd8540e6e39e1f0e59a92e7e24
mehmethakan0220 <mehmethakan0220@gmail.com>
2022-09-04 15:34:19 +0300

    Boolean attributes

--- 1-intro/1-fundamentals/src/App.vue
+++ 1-intro/1-fundamentals/src/App.vue
@@ -7,6 +7,10 @@
   <div v-bind:id="dynamicId"> If the bound value is null or undefined, 
     then the attribute will be removed from the rendered element. try as both null and undefined</div>
 
+    <button :disabled="disabledButton">button1</button>
+    <button :disabled="enabledButton">button2</button>
+    <button :disabled="defaultDisabled">button3</button>
+
 </template>
 
 <script>
@@ -19,6 +23,9 @@ export default  {
       dynamicId:112,/*
       The v-bind directive instructs Vue to keep the element's id attribute in sync with the component's dynamicId property. If the bound value is null or undefined, then the attribute will be removed from the rendered element.
       */
+      disabledButton: true,
+      enabledButton:  false,
+      defaultDisabled: '', //in html disabled attribute without value equals disable the button
     }
   }
 };
