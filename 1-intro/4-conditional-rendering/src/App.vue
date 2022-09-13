8210e8d0feaf89cb213996f8db2ab7c340c4c4f9
mehmethakan0220 <mehmethakan0220@gmail.com>
2022-09-13 21:55:58 +0300

    v-show

--- 1-intro/4-conditional-rendering/src/App.vue
+++ 1-intro/4-conditional-rendering/src/App.vue
@@ -30,6 +30,22 @@
   <template v-else>
     <div>template for v-else depends on isOk's falsy values</div>
   </template>
+
+  <hr>
+  <h2>v-show</h2>
+  <br>
+  <div>Another option for conditionally displaying an element is the v-show directive. The usage is largely the same:</div>
+  <pre>
+    &lth1 v-show="ok"&gtHello!&lt/h1&gt
+  </pre>
+  <hr>
+  <div>The difference is that an element with v-show will always be rendered and remain in the DOM; v-show only toggles the display CSS property of the element. v-show doesn't support the &lttemplate&gt element, nor does it work with v-else.</div>
+  <hr>
+  <button @click="disp = !disp">click to hide below div</button><br>
+  <div v-show="disp">When clicking button this this will hide, with display:none </div>
+  <br>
+  <br>
+
 </template>
 <script>
 export default{
@@ -37,6 +53,7 @@ export default{
     return{
       condition:true,
       isOk:false,
+      disp:true,
     }
   }
 }
